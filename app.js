93926f529f5a637fe817fd2191034997c5e4c4d1
fanu85 <fanuelngonga@gmail.com>
2020-09-10 22:26:14 +0300

    edited manifest file and registered service worker

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 const apiKey = '9ddf57db420ad015c1b7cc510e3e152d';
 const baseUrl ='https://api.openweathermap.org/data/2.5/';
+
+
+
+
+window.addEventListener('load', (evt)=>{
+
+    if('serviceWorker' in navigator){
+        navigator.serviceWorker.register('/sw.js')
+        .then(register=>{
+            console.log('Service Worker Successfully registeed!');
+        })
+        .catch(()=>{ console.log('Service worker registration failed')});
+    }
+    else{
+        console.log('Service worker not supported!');
+    }
+
+});
